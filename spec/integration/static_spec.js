85f42cfce5054965a55fae65287ee30c93f99982
Arata Kagan <aratakagan@gmail.com>
2018-11-05 22:55:34 -0800

    intro express assignment finished

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,20 +3,33 @@ const request = require("request");
 const server = require("../../src/server");
 
 const base = "http://localhost:3000/";
+const base2 = "http://localhost:3000/marco";
+
+// describe("routes : static", () => {
+
+//     describe("GET /", () => {
+
+//         it("should return status code 200", (done) => {
+//             //passing done tells Jasmine to wait until expect is called
+//             // if we remove done and done() method call, the test will pass 
+//             //since Jasmine assumes no expect means success 
+
+//             request.get(base, (err, res, body) => {
+
+//                 expect(res.statusCode).toBe(200);
+
+//                 done();
+//             });
+//         });
+//     });
+// }); 
 
 describe("routes : static", () => {
-
-    describe("GET /", () => {
-
+    describe("GET /marco", () => {
         it("should return status code 200", (done) => {
-            //passing done tells Jasmine to wait until expect is called
-            // if we remove done and done() method call, the test will pass 
-            //since Jasmine assumes no expect means success 
-
-            request.get(base, (err, res, body) => {
-
+            request.get(base2, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-
+                expect(res.body).toBe("polo");
                 done();
             });
         });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    //define route using use method passing a pattern and callback
-    // The callback will be fired when the request is handled 
-    res.send("Welcome to Bloccit");
+// router.get("/", (req, res, next) => {
+//     //define route using use method passing a pattern and callback
+//     // The callback will be fired when the request is handled 
+//     res.send("Welcome to Bloccit");
+// });
+
+router.get("/marco", (req, res, next) => {
+    res.send("polo");
 });
 
 module.exports = router;
