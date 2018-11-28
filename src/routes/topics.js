b69c31f772e4ea4988ed97ba3562b8acd175fd54
Arata Kagan <aratakagan@gmail.com>
2018-11-27 16:50:03 -0800

    validation assignment done

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const topicController = require("../controllers/topicController")
+const validation = require("./validation");
 
 //if the topics route is selected, return index function from controller
 router.get("/topics", topicController.index);
@@ -9,7 +10,7 @@ router.get("/topics", topicController.index);
 //if the topics/new route is selected, return new function from controller
 router.get("/topics/new", topicController.new);
 
-router.post("/topics/create", topicController.create);
+router.post("/topics/create", validation.validateTopics, topicController.create);
 
 router.get("/topics/:id", topicController.show);
 
@@ -17,7 +18,7 @@ router.post("/topics/:id/destroy", topicController.destroy);
 
 router.get("/topics/:id/edit", topicController.edit);
 
-router.post("/topics/:id/update", topicController.update);
+router.post("/topics/:id/update", validation.validateTopics, topicController.update);
 
 
 module.exports = router;
