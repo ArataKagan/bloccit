10e19c7460c3f22b546a7ad7d44c26448abbfdc8
Arata Kagan <aratakagan@gmail.com>
2018-11-30 15:03:51 -0800

    authorization checkpoint done

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,88 +1,100 @@
-const topicQueries = require("../db/queries.topics.js")
+const topicQueries = require("../db/queries.topics.js");
+const Authorizer = require("../policies/topic");
 
 module.exports = {
     index(req, res, next){
-     
-    topicQueries.getAllTopics((err, topics) => {
-      if(err){
-        res.redirect(500, "static/index");
-      } else {
-        res.render("topics/index", {topics});
-      }
-    })
-    },
+        topicQueries.getAllTopics((err, topics) => {
 
+            if(err){
+                res.redirect(500, "static/index");
+            } else {
+                res.render("topics/index", {topics});
+            }
+            });
+    },
+    
     new(req, res, next){
-      res.render("topics/new");
+        const authorized = new Authorizer(req.user).new();
+    
+        if(authorized) {
+            res.render("topics/new");
+        } else {
+            req.flash("notice", "You are not authorized to do that.");
+            res.redirect("/topics");
+        }
     },
 
     create(req, res, next){
-      //grabs the value from the form from the body property of the request
-      let newTopic = {
-        title: req.body.title,
-        description: req.body.description
-      };
-      //assign the grabbed new topic to addTopic method
-      topicQueries.addTopic(newTopic, (err, topic) => {
-        if(err){
-          //if failed, send an error code
-          res.redirect(500, "/topics/new");
-        } else { 
-          //if success, redirect to show view for the new topic
-          res.redirect(303, `/topics/${topic.id}`);
+
+        const authorized = new Authorizer(req.user).create();
+    
+        if(authorized) {
+            let newTopic = {
+            title: req.body.title,
+            description: req.body.description
+            };
+            topicQueries.addTopic(newTopic, (err, topic) => {
+            if(err){
+                res.redirect(500, "topics/new");
+            } else {
+                res.redirect(303, `/topics/${topic.id}`);
+            }
+            });
+        } else {
+    
+            req.flash("notice", "You are not authorized to do that.");
+            res.redirect("/topics");
         }
-      });
     },
 
     show(req, res, next){
-      // use req.params.id instead of req.body since we need the URL
-      //for the case of topics/5, the value 5 is stored in a key called id 
-      // in the params property of the request 
-      topicQueries.getTopic(req.params.id, (err, topic) => {
-
-        // if err or topic returns nothing, redirect to the root
-        if(err || topic == null){
-          res.redirect(404, "/");
-        //otherwise, render the show partial and pass in the topic to render
-        } else {
-          res.render("topics/show", {topic});
-        }
-      });
+        topicQueries.getTopic(req.params.id, (err, topic) => {
+            if(err || topic == null){
+                res.redirect(404, "/");
+            } else {
+                res.render("topics/show", {topic});
+            }
+        });
     },
 
     destroy(req, res, next){
-      topicQueries.deleteTopic(req.params.id, (err, topic) => {
-        if(err){
-          //On err, return a server err and redirect to the show view
-          res.redirect(500, `/topics/${topic.id}`)
-        } else { 
-          //On success, redirect to the /topics path
-          res.redirect(303, "/topics")
-        }
-      });
+
+        topicQueries.deleteTopic(req, (err, topic) => {
+            if(err){
+            res.redirect(err, `/topics/${req.params.id}`)
+            } else {
+            res.redirect(303, "/topics")
+            }
+        });
     },
 
     edit(req, res, next){
-      topicQueries.getTopic(req.params.id, (err, topic) => {
-        if(err || topic == null){
-          res.redirect(404, "/");
-        } else {
-          res.render("topics/edit", {topic});
-        }
-      });
+
+        topicQueries.getTopic(req.params.id, (err, topic) => {
+            if(err || topic == null){
+            res.redirect(404, "/");
+            } else {
+    
+            const authorized = new Authorizer(req.user, topic).edit();
+    
+            if(authorized){
+                res.render("topics/edit", {topic});
+            } else {
+                req.flash("You are not authorized to do that.")
+                res.redirect(`/topics/${req.params.id}`)
+            }
+            }
+        });
     },
 
     update(req, res, next){
-      topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
-        if(err || topic == null){
-          res.redirect(404, `/topics/${req.params.id}/edit`);
-        } else {
-          res.redirect(`/topics/${topic.id}`);
-        }
-      });
+
+        topicQueries.updateTopic(req, req.body, (err, topic) => {
+            if(err || topic == null){
+            res.redirect(401, `/topics/${req.params.id}/edit`);
+            } else {
+            res.redirect(`/topics/${req.params.id}`);
+            }
+        });
     }
-
-
-
-
-  }
+}
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -3,6 +3,7 @@
 const Topic = require("./models").Topic;
 const Post = require("./models").Post;
 const Flair = require("./models").Flair;
+const Authorizer = require("../policies/topic");
 
 module.exports = {
     // function retun the result of calling all the topics
@@ -52,35 +53,49 @@ module.exports = {
         })
     },
 
-    deleteTopic(id, callback){
-        return Topic.destroy({
-            where: {id}
-        })
+    deleteTopic(req, callback){
+        return Topic.findById(req.params.id)
         .then((topic) => {
-            callback(null, topic);
+            const authorized = new Authorizer(req.user, topic).destroy();
+
+            if(authorized){
+                topic.destroy()
+                .then((res) => {
+                    callback(null, topic);
+                });
+            } else {
+                req.flash("notice", "You are not authorized to do that.")
+                callback(401);
+            }
         })
         .catch((err) => {
             callback(err);
-        })
+        });
     },
 
-    updateTopic(id, updatedTopic, callback){
-        return Topic.findById(id)
+    updateTopic(req, updatedTopic, callback){
+        return Topic.findById(req.params.id)
         .then((topic) => {
             if(!topic){
                 return callback("Topic not found");
             }
 
-            topic.update(updatedTopic, {
-                // passing array of keys to the fields property
-                fields: Object.keys(updatedTopic)
-            })
-            .then(() => {
-                callback(null, topic);
-            })
-            .catch((err) => {
-                callback(err);
-            });
+            const authorized = new Authorizer(req.user, topic).update();
+
+            if(authorized){
+                topic.update(updatedTopic, {
+                    fields: Object.keys(updatedTopic)
+                })
+                .then(() => {
+                    callback(null, topic);
+                })
+                .catch((err) => {
+                    callback(err);
+                });
+            } else {
+                req.flash("notice", "You are not authorized to do that.");
+                callback("Forbidden");
+            }
         });
     }
 }
