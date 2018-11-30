a0082358667f811e71430e6f6c85937b566d8f62
Arata Kagan <aratakagan@gmail.com>
2018-11-30 15:19:57 -0800

    add-user migration file modifed

--- src/db/migrations/20181129193619-add-user-to-posts.js
+++ src/db/migrations/20181129193619-add-user-to-posts.js
@@ -1,33 +1,25 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    
+
     return queryInterface.addColumn(
       "Posts",
       "userId",
       {
         type: Sequelize.INTEGER,
         onDelete: "CASCADE",
-        // posts will not exist without a user so we set allowNull to false
         allowNull: false,
         references: {
           model: "Users",
           key: "id",
           as: "userId"
-        }, 
+        },
       }
     );
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn("Posts", "userId");
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
