9c93f45a6bc3875573fd814ba1e4fbadc54cc6bf
Arata Kagan <aratakagan@gmail.com>
2018-12-05 10:01:48 -0800

    Fixed comment new.ejs template

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -45,7 +45,7 @@ module.exports = {
           req.checkBody("email", "must be valid").isEmail();
           req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
           req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
-      }
+      };
       
       const errors = req.validationErrors();
 
