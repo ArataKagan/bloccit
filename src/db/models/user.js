d15c1aac512f1633c2d693e06fc47cc734ec2c36
Arata Kagan <aratakagan@gmail.com>
2018-12-06 11:06:15 -0800

    voting checkpoint done

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -29,7 +29,14 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       as: "comments"
     });
+
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
   };
+
+  
 
   User.prototype.isAdmin = function() {
     return this.role === "admin";
