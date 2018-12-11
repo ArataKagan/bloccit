7f3eb6c1dba35e6a059f7d4d34453c4b0d503435
Arata Kagan <aratakagan@gmail.com>
2018-12-10 16:36:07 -0800

    Changed rule to post and comment

--- src/policies/post.js
+++ src/policies/post.js
@@ -1,14 +1,6 @@
 const ApplicationPolicy = require("./application");
 
 module.exports = class PostPolicy extends ApplicationPolicy {
-    new(){
-        return this.user != null;
-    }
-
-    create(){
-        return this.new();
-    }
-
     edit(){
         return this._isOwner() || this._isAdmin();
     }
--- src/policies/topic.js
+++ src/policies/topic.js
@@ -1,18 +1,7 @@
- // #1
- const ApplicationPolicy = require("./application");
+const ApplicationPolicy = require("./application");
 
- module.exports = class TopicPolicy extends ApplicationPolicy {
+module.exports = class TopicPolicy extends ApplicationPolicy {
  
-  // #2
-   new() {
-     return this._isAdmin();
-   }
- 
-   create() {
-     return this.new();
-   }
- 
-  // #3
    edit() {
      return this._isAdmin();
    }
