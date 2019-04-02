dfb864e853c5194e622ee299e14daad849c1020c
Arata Kagan <aratakagan@gmail.com>
2019-04-02 14:32:03 -0700

    Clean up static routing file

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,18 +1,11 @@
-// Define static routers in this file
-// import Express router instance and get matching route path 
-
 const app = require("../app");
 const express = require("express");
-//instantiate router 
 const router = express.Router(); 
 
 const staticController = require("../controllers/staticController");
 
-//index page routing
 router.get("/", staticController.index); 
 
-//about page routing
 router.get("/about", staticController.about);
-
 
 module.exports = router;
